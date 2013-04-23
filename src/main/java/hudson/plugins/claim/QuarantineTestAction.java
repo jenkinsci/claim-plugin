090c85c2233865e1b4d2cfb87725ebffac4c1a45
sam nobs <samuel.nobs@taitradio.com>
2013-04-23 15:37:36 +1200

    add forgotten file

--- src/main/java/hudson/plugins/claim/QuarantineTestAction.java
+++ src/main/java/hudson/plugins/claim/QuarantineTestAction.java
@@ -1,21 +1,32 @@
 package hudson.plugins.claim;
 
-import hudson.plugins.claim.ClaimTestDataPublisher.Data;
+import hudson.plugins.claim.QuarantineTestDataPublisher.Data;
 import hudson.model.BuildBadgeAction;
 import hudson.model.Hudson;
 import hudson.model.ProminentProjectAction;
 import hudson.model.Saveable;
 import hudson.model.User;
 import hudson.tasks.junit.TestAction;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
+import java.util.Date;
 
+@ExportedBean(defaultVisibility = 2)
 public class QuarantineTestAction 
 	extends TestAction 
 	implements BuildBadgeAction, ProminentProjectAction
 {
-
+	private boolean quarantined;
+	private String quarantinedBy;
+	private Date quarantineDate;
+	private String reason;
+    	
+	protected Data owner;
 	
 	QuarantineTestAction(Data owner, String testObjectId) {
+		this.owner = owner;
 	}
+
 
 	public String getDisplayName() {
 		return Messages.QuarantineTestAction_DisplayName();
@@ -29,14 +40,20 @@ public class QuarantineTestAction
 		return "quarantine";
 	}	
 	
-//	@Override
-//	public void claim(String claimedBy, String reason, boolean sticky) {
-//		super.claim(claimedBy, reason, sticky);
-//		owner.addClaim(testObjectId, this);
-//	}
-	
 	public String getNoun() {
 		return Messages.QuarantineTestAction_Noun();
+	}
+	
+	@Exported
+	public boolean isQuarantined() {
+		return quarantined;
+	}
+
+	public void quarantine(String quarantinedBy, String reason) {
+		this.quarantined = true;
+		this.quarantinedBy = quarantinedBy;
+		this.reason = reason;
+		this.quarantineDate = new Date();
 	}
 
 }

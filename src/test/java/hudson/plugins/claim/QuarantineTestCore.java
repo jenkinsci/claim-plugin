23d53d01e6b851b77ae340fd7e6720f802b7a7a3
sam nobs <samuel.nobs@taitradio.com>
2013-06-18 11:48:10 +1200

    seems like my attempt to put common stuff in a base class for testing makes the tests not show up in the results on buildhive. so i have now removed the base class and introduced some duplication - can't figure out how to work around multiple inheritance, looks like i can't help thinking C++ when writing Java

--- src/test/java/hudson/plugins/claim/QuarantineTestCore.java
+++ src/test/java/hudson/plugins/claim/QuarantineTestCore.java
@@ -22,11 +22,11 @@
  * THE SOFTWARE.
  */
 package hudson.plugins.claim;
-
-
 import hudson.model.FreeStyleBuild;
 import hudson.model.Result;
 import hudson.model.Descriptor;
+import hudson.model.FreeStyleProject;
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy;
 import hudson.tasks.junit.TestDataPublisher;
 import hudson.tasks.junit.TestResult;
 import hudson.tasks.junit.SuiteResult;
@@ -35,9 +35,53 @@ import hudson.tasks.junit.JUnitResultArchiver;
 import hudson.tasks.junit.TestResultAction;
 import hudson.util.DescribableList;
 
+import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.TestBuilder;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import java.io.IOException;
 
+public class QuarantineTestCore extends HudsonTestCase {
+	private String projectName = "x";
+	protected String quarantineText = "quarantineReason";
+	protected FreeStyleProject project;
 
-public class QuarantineTestCore extends QuarantineTest {
+	@Override
+	protected void setUp() throws Exception {
+	    super.setUp();
+	    java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.SEVERE);
+	    
+		project = createFreeStyleProject(projectName);
+	    DescribableList<TestDataPublisher, Descriptor<TestDataPublisher>> publishers =
+	        new DescribableList<TestDataPublisher, Descriptor<TestDataPublisher>>(project);
+	    publishers.add(new QuarantineTestDataPublisher());
+	    project.getPublishersList().add(new QuarantinableJUnitResultArchiver("*.xml",false, publishers));
+		
+	    hudson.setAuthorizationStrategy(new FullControlOnceLoggedInAuthorizationStrategy());
+	    hudson.setSecurityRealm(createDummySecurityRealm());
+	}
+
+	protected FreeStyleBuild runBuildWithJUnitResult(final String xmlFileName) throws Exception {
+		FreeStyleBuild build;
+	    project.getBuildersList().add(new TestBuilder() {
+	        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+	            build.getWorkspace().child("junit.xml").copyFrom(
+	                getClass().getResource(xmlFileName));
+	            return true;
+	        }
+	    });
+	    build = project.scheduleBuild2(0).get();
+	    project.getBuildersList().clear();
+	    return build;
+	}
+
+	protected TestResult getResultsFromJUnitResult(final String xmlFileName) throws Exception {
+		return runBuildWithJUnitResult(xmlFileName).getAction(TestResultAction.class).getResult();
+	}
+
+	
+	
 	
 	public void testAllTestsHaveQuarantineAction() throws Exception {
     	TestResult tr = getResultsFromJUnitResult("junit-1-failure.xml");

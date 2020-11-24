4650366c4406f7ad4975249e3fa3259139a10448
Marc Guillemot <mguillemot@yahoo.fr>
2020-11-24 13:07:03 +0100

    add report on job & user level, add claimed failed tests in reports
    and API to get number of unclaimed failed tests for a job

---
+++ src/main/java/hudson/plugins/claim/AbstractAssignedClaimsReport.java
@@ -0,0 +1,127 @@
+package hudson.plugins.claim;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.jelly.JellyContext;
+import org.jenkins.ui.icon.Icon;
+import org.jenkins.ui.icon.IconSet;
+import org.jenkins.ui.icon.IconSpec;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.DoNotUse;
+import org.kohsuke.stapler.Stapler;
+
+import hudson.model.Action;
+import hudson.model.Job;
+import hudson.model.ModelObject;
+import hudson.model.Run;
+import hudson.model.View;
+import hudson.tasks.test.AbstractTestResultAction;
+import hudson.tasks.test.TestResult;
+import hudson.util.RunList;
+import jenkins.model.Jenkins;
+
+public class AbstractAssignedClaimsReport implements Action, IconSpec {
+    @Override
+    public String getIconClassName() {
+        return "icon-claim-claim";
+    }
+
+    public String getIconFileName() {
+        String iconClassName = getIconClassName();
+        if (iconClassName != null) {
+            Icon icon = IconSet.icons.getIconByClassSpec(iconClassName + " icon-md");
+            if (icon != null) {
+                JellyContext ctx = new JellyContext();
+                ctx.setVariable("resURL", Stapler.getCurrentRequest().getContextPath() + Jenkins.RESOURCE_PATH);
+                return icon.getQualifiedUrl(ctx);
+            }
+        }
+        return null;
+    }
+
+    public String getUrlName() {
+        return "/claims";
+    }
+
+    @Restricted(DoNotUse.class) // jelly only
+    public Run getFirstFail(final Run r) {
+        Run lastGood = r.getPreviousNotFailedBuild();
+        Run firstFail;
+        if (lastGood == null) {
+            firstFail = r.getParent().getFirstBuild();
+        } else {
+            firstFail = lastGood.getNextBuild();
+        }
+        return firstFail;
+    }
+
+    @Restricted(DoNotUse.class) // jelly only
+    public CommonMessagesProvider getMessageProvider(final Run r) {
+        return CommonMessagesProvider.build(getAction(r));
+    }
+
+    @Restricted(DoNotUse.class) // jelly only
+    public CommonMessagesProvider getMessageProvider(final TestResult tr) {
+        return CommonMessagesProvider.build(tr.getTestAction(ClaimTestAction.class));
+    }
+
+    public ModelObject getOwner() {
+        View view = Stapler.getCurrentRequest().findAncestorObject(View.class);
+        if (view != null) {
+            return view;
+        } else {
+            return Jenkins.getInstance().getStaplerFallback();
+        }
+    }
+
+    private ClaimBuildAction getAction(final Run r) {
+        return ClaimUtils.getBuildAction(r, false);
+    }
+
+    public RunList getBuilds() {
+        List<Run> lastBuilds = new ArrayList<>();
+        for (Job job : getJobs()) {
+            Run lb = job.getLastCompletedBuild();
+            if (lb != null && isDisplayed(lb.getAction(ClaimBuildAction.class))) {
+                lastBuilds.add(lb);
+            }
+        }
+
+        return RunList.fromRuns(lastBuilds).failureOnly();
+    }
+
+	protected List<Job> getJobs() {
+		return Jenkins.getInstance().getAllItems(Job.class);
+	}
+
+    public List<TestResult> getTestFailuresWithClaim() {
+        List<TestResult> claimedTestFailures = new ArrayList<>();
+        for (Job job : getJobs()) {
+            Run lb = job.getLastCompletedBuild();
+            if (lb != null) {
+				AbstractTestResultAction testResultAction = lb.getAction(AbstractTestResultAction.class);
+				if (testResultAction != null) {
+		            List<? extends TestResult> failedTests = testResultAction.getFailedTests();
+		            for (TestResult failedTest : failedTests) {
+		                ClaimTestAction claimAction = failedTest.getTestAction(ClaimTestAction.class);
+		                if (isDisplayed(claimAction)) {
+		                    claimedTestFailures.add(failedTest);
+		                }
+		            }
+				}
+            }
+        }
+
+        return claimedTestFailures;
+    }
+    
+    protected boolean isDisplayed(AbstractClaimBuildAction<?> claimAction) {
+    	return claimAction != null && claimAction.isClaimed();
+    }
+
+    public String getDisplayName() {
+        return Messages.ClaimedBuildsReport_DisplayName();
+    }
+
+}
---
+++ src/test/java/hudson/plugins/claim/UserClaimsReportTest.java
@@ -0,0 +1,83 @@
+package hudson.plugins.claim;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.FailureBuilder;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import hudson.model.FreeStyleProject;
+import hudson.model.User;
+
+public class UserClaimsReportTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Before
+    public void setUp() throws Exception {
+
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.SEVERE);
+    }
+
+    private FreeStyleProject createFailingJobWithName(String jobName) throws IOException,
+            InterruptedException, ExecutionException {
+        FreeStyleProject project = j.createFreeStyleProject(jobName);
+        project.getBuildersList().add(new FailureBuilder());
+        project.getPublishersList().add(new ClaimPublisher());
+        project.scheduleBuild2(0).get();
+        return project;
+    }
+
+    @Test
+    public void userAssignedFailedJobsShouldBeVisibleInUserClaimReport() throws Exception {
+    	long timestamp = System.currentTimeMillis();
+
+    	FreeStyleProject job1 = createFailingJobWithName("job1-" + timestamp);
+    	FreeStyleProject job2 = createFailingJobWithName("job2-" + timestamp);
+    	FreeStyleProject job3 = createFailingJobWithName("job3-" + timestamp);
+        
+        User userA = User.get("userA-" + timestamp, true, Collections.emptyMap());
+        User userB = User.get("userB-" + timestamp, true, Collections.emptyMap());
+        User userC = User.get("userC-" + timestamp, true, Collections.emptyMap());
+
+        // none claimed
+        verifyUserClaims(userA, 0, 0);
+        verifyUserClaims(userB, 0, 0);
+        verifyUserClaims(userC, 0, 0);
+
+        // job1 claimed by A
+        ClaimBuildAction claimAction1 = job1.getLastBuild().getAction(ClaimBuildAction.class);
+        claimAction1.applyClaim(userA.getId(), "test reason", userB.getId(), new Date(), true, true);
+        verifyUserClaims(userA, 1, 0);
+        verifyUserClaims(userB, 0, 0);
+        verifyUserClaims(userC, 0, 0);
+
+        // job2 claimed by C
+        ClaimBuildAction claimAction2 = job2.getLastBuild().getAction(ClaimBuildAction.class);
+        claimAction2.applyClaim(userC.getId(), "test reason", userB.getId(), new Date(), true, true);
+        verifyUserClaims(userA, 1, 0);
+        verifyUserClaims(userB, 0, 0);
+        verifyUserClaims(userC, 1, 0);
+
+        // job3 claimed by C
+        ClaimBuildAction claimAction3 = job3.getLastBuild().getAction(ClaimBuildAction.class);
+        claimAction3.applyClaim(userC.getId(), "test reason", userA.getId(), new Date(), true, true);
+        verifyUserClaims(userA, 1, 0);
+        verifyUserClaims(userB, 0, 0);
+        verifyUserClaims(userC, 2, 0);
+}
+
+    private void verifyUserClaims(User user, int nbProjectClaims, int nbTestFailureClaims) throws Exception {
+        HtmlPage page = j.createWebClient().goTo("user/" + user.getId() + "/claims/");
+        j.assertXPathValue(page, "id('claim-nb-project-claims')", String.valueOf(nbProjectClaims));
+        j.assertXPathValue(page, "id('claim-nb-testfailure-claims')", String.valueOf(nbTestFailureClaims));
+	}
+}

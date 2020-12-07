2c6cb0b04d2121eb647b64ff4644fac5781ca336
Marc Guillemot <mguillemot@yahoo.fr>
2020-12-07 08:56:25 +0100

    add pipeline property allowBrokenBuildClaiming()

---
+++ src/main/java/hudson/plugins/claim/AllowBrokenBuildClaimingJobProperty.java
@@ -0,0 +1,36 @@
+package hudson.plugins.claim;
+
+import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.export.ExportedBean;
+
+import hudson.Extension;
+import hudson.model.Job;
+import jenkins.model.OptionalJobProperty;
+
+/**
+ * {@link OptionalJobProperty} for to allow broken builds to be "claimable".
+ */
+@ExportedBean
+public class AllowBrokenBuildClaimingJobProperty extends OptionalJobProperty<WorkflowJob> {
+
+    @DataBoundConstructor
+    public AllowBrokenBuildClaimingJobProperty() {
+    }
+
+    @Extension(optional = true)
+    @Symbol("allowBrokenBuildClaiming")
+    public static class DescriptorImpl extends OptionalJobPropertyDescriptor {
+
+        @Override public String getDisplayName() {
+            return Messages.ClaimPublisher_DisplayName();
+        }
+
+    }
+
+    public static boolean isClaimable(Job<?, ?> job) {
+        return job.getProperty(AllowBrokenBuildClaimingJobProperty.class) != null;
+    }
+
+}

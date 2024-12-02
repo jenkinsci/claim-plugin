01eeca9dc3f0c7f12ff7bfcf166c21507cc037b3
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-12-02 15:30:32 +0200

    [JENKINS-74109] Extract inline JavaScript from `AbstractClaimBuildAction/summary.jelly`

---
+++ src/main/resources/hudson/plugins/claim/AbstractClaimBuildAction/resource.js
@@ -0,0 +1,42 @@
+function showPopup(hoveritem) {
+    var hp = document.getElementById("claimHoverPopup");
+    hp.style.display = "block";
+}
+
+function hidePopup() {
+    var hp = document.getElementById("claimHoverPopup");
+    hp.style.display = "none";
+}
+
+function display(error) {
+    var reasonText = document.getElementById("errordesc");
+    claimBuildAction.getReason(error, function(content) {
+        reasonText.textContent = content.responseObject();
+    });
+}
+
+Behaviour.specify("A#claim", "AbstractClaimBuildAction_claim", 0, function (element) {
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
+        showPopup(event.target.closest("a#claim"));
+    });
+});
+
+Behaviour.specify("A#reassign", "AbstractClaimBuildAction_reassign", 0, function (element) {
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
+        showPopup(event.target.closest("a#reassign"));
+    });
+});
+
+Behaviour.specify(".claim-hide-popup", "AbstractClaimBuildAction_hide-popup", 0, function (element) {
+    element.addEventListener("click", (event) => {
+        hidePopup();
+    });
+});
+
+Behaviour.specify(".claim-bfa-display-error", "AbstractClaimBuildAction_display-bfa-error", 0, function (element) {
+    element.addEventListener("change", (event) => {
+        display(event.target.value);
+    });
+});
